1b8443f66083703b8f645d692a8f32dd9ca428db
MosheL2680 <moshelubetski@gmail.com>
2023-11-20 17:55:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  const [n1, setn1] = useState(0);
+  const [n2, setn2] = useState(0);
+  const [res, setRes] = useState(0);
+  const calc = (opr) => { setRes(eval(`${n1} ${opr} ${n2}`)) }
+  const color1 = { backgroundColor: `rgb(${res % 255},${res % 255},${res % 255})` }
+  const color2 = { backgroundColor: `rgb(${res * 2 % 255},${res % 255},${res % 255})` }
+  const color3 = { backgroundColor: `rgb(${res * 3 % 255},${res % 255},${res % 255})` }
+  const color4 = { backgroundColor: `rgb(${res * 4 % 255},${res % 255},${res % 255})` }
+  return (<div>
+    <input onChange={(e) => setn1(e.target.value)} />
+    <input onChange={(e) => setn2(e.target.value)} /><br />
+    <button onClick={() => calc('+')} style={color1}>+</button>
+    <button onClick={() => calc('-')} style={color2}>-</button>
+    <button onClick={() => calc('*')} style={color3}>*</button>
+    <button onClick={() => calc('/')} style={color4}>/</button>
+    <h1>Result: {res}</h1>
+  </div>)
+} export default App;
